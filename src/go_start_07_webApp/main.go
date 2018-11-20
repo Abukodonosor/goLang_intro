d6b283c5084e1f09d255078d06f8054527779997
Abukodonosor <cube.uros@gmail.com>
2018-11-20 12:10:17 +0100

    added options to go_webAp

--- src/go_start_07_webApp/main.go
+++ src/go_start_07_webApp/main.go
@@ -1,17 +1,20 @@
 package main
 
+// https://medium.com/@ScullWM/golang-http-server-for-pro-69034c276355
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Gde si decko !!")
+	//r.URL.Query().Get("keyword")
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
-func newHandler(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Hi this is new route!!")
 
 	var HTML string
@@ -19,8 +22,35 @@ func newHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s", HTML)
 }
 
+func manuHandler(w http.ResponseWriter, r *http.Request) {
+
+	log.Println("Usao u menuHandler!!!")
+	data, err := ioutil.ReadFile("./index.html")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprintf(w, "%s", string(data))
+}
+
+//midleware
+// info https://www.alexedwards.net/blog/making-and-using-middleware
+func middlewareOne(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Executing middlewareOne")
+		next.ServeHTTP(w, r)
+		log.Println("Executing middlewareOne again")
+	})
+}
+
 func main() {
+	// serve static content
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// declare routes of webApp
 	http.HandleFunc("/", handler)
-	http.HandleFunc("/home", newHandler)
+	http.HandleFunc("/home", homeHandler)
+
+	http.HandleFunc("/menu", manuHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
